53c46fa54002341e2f3ae19bd8942158d5798357
Julia Kerner <juliakerner@gmail.com>
2016-11-06 21:55:23 -0500

    Fix script.js

--- Modul4_solutions/script.js
+++ Modul4_solutions/script.js
@@ -1,358 +1,87 @@
-
-// Lesson40
-var x = "Hello World!";
-
-
-// Lesson41
-var message = "in global";
-console.log("global: message = "+ message);
-
-var a = function () {
-	var message = "inside a";
-	console.log("a: message = "+ message);
-function b () {
-    console.log("b: message = " + message);
- }
-
-	b();
-}
-a();
-
-
-//Lesson42
-var y;
-console.log(y);
-
-if (y == undefined) {
-	console.log("y is undefined");
-}
-
-y = 5;
-if (y == undefined) {
-	console.log("y is undefined");
-}
-else {
-	console.log("y has been defined");
-}
-
-//Lesson43 *** String concatination
-
-// var string = "Hello";
-// string += " World";
-// // string = string + " World";
-// console.log(string + "!");
-
-//*** Regular math operators: +. -, *, /
-// console.log((5 + 4) / 3);
-// console.log(undefined /5);
-// function test1 (a) {
-// 	console.log(a / 5);
-// }
-// test1();
-
-//*** Equality
-// var a = 4, b = 4;
-// if (a == b) {
-// 	console.log("a=4 is equal to b=4");
-// }
-
-// a = "4";
-// if (a == b) {
-// 	console.log("a='4' is equal to b=4");
-// }
-
-// // // *** Strict equality
-// if (a === b) {
-// 	console.log("Strict: a='4' is equal to b=4");
-// }
-// else {
-// 	console.log("Strict: a='4' is NOT equal to b=4");
-// }
-
-//*** If statement (all false)
-// if (false || null || undefined || "" || 0 || NaN) {
-// 	console.log("this line will never execute");
-// }
-// else {
-// 	console.log("All false");
-// }
-//*** If statement (all true)
-// if (true && "Hello" && 1 && -1 && 'false') {
-// 	console.log("all true");
-// }
-
-// // ***Best practice for {} style
-// // Same line or next?
-// // is it just a style? - IT IS NOT! Second way is correct.
-// function a()
-// {
-// 	return 
-// 	{
-// 		name: "Julia";
-// 	};
-// }
-
-// function b() {
-// 	return {
-// 		name: "Julia"
-// 	};
-// }
-
-// console.log(a());
-// console.log(b());
-
-// For loop
-// var sum = 0;
-// for (var i = 0; i < 10; i++) {
-// 	console.log(i);
-// 	sum = sum +i;
-// }
-// console.log("sum of 0 through 9 is: " + sum);
-
-// // *** Lesson44 - setting default value
-// function orderChickenWith(sideDish) {
-//   //if (sideDish === undefined) {
-//   //	sideDish = "whatever!"; ----- OR
-//   sideDish = sideDish || "whatever!";	
-//   console.log("Chicken with " + sideDish);
-// }
-
-// orderChickenWith("noodles");
-// orderChickenWith();
-
-// // *** Lesson45 - object creation
-// var company = new Object();
-// company.name = "facebook";
-// company.ceo = new Object();
-// company.ceo.firstName = "Mark";
-// company.ceo.favColor = "blue";
-
-// console.log(company);
-// console.log("Company CEO name is: " 
-// 	+ company.ceo.firstName);
-
-// console.log(company["name"]);
-// var stockPropName = "stock of the company";
-// company[stockPropName] = 110;
-
-// console.log("Stock price is: " +
-// 	company[stockPropName]);	
-
-// Better way: object literal
-
-var facebook = {
-	name: "facebook",
-	ceo: {
-		firstName: "Mark",
-		favColor: "blue"
-	},
-	"Stock of company": 110
-}
-
-console.log(facebook);
-
-// *** Lesson46
-// Functions are First-Class Data Types
-// Functions ARE objects
-
-function multiply(x, y) {
-	return x * y;
-}
-
-multiply.version = "v.1.0.0";
-console.log(multiply.version);
-
-// Function factory
- function makeMultiplier(multiplier) {
- 	var myFunc = function(x) {
- 		return multiplier * x;
- 	}
-
- 	return myFunc;
- }
-
- var multiplyBy3 = makeMultiplier(3);
- console.log(multiplyBy3(10));
- var doubleAll = makeMultiplier(2);
- console.log(doubleAll(100));
-
- // Passing functions as arguments
-
- function doOperationOn(x, operation) {
- return operation(x);	
- }
-
- var result = doOperationOn(5, multiplyBy3);
- console.log(result);
- result = doOperationOn(100, doubleAll);
- console.log(result);
-
- //***Lesson47
-
- //Copy by Reference vs by Value
-
-// var a = 7;
-// var b = a;
-// console.log("a: " + a);
-// console.log("b: " + b);
-
-// b = 5;
-// console.log("after b undate:");
-// console.log("a: " + a);
-// console.log("b: " + b); 
-
-// var a = { x: 7 };
-// var b = a;
-// console.log(a);
-// console.log(b);
-
-// b.x = 5;
-// console.log("after b.x undate:");
-// console.log(a);
-// console.log(b);
-
-// // Pass by reference vs by value
-// function changePrimitive(primValue) {
-// 	console.log("in changePrimitive...");
-// 	console.log("before:");
-// 	console.log(primValue);
-
-// 	primValue = 5;
-// 	console.log("after:");
-// 	console.log(primValue);
-// }
-//  var value = 7;
-//  changePrimitive(value); // primValue = value
-//  console.log("after changePrimitive, orig value:");
-//  console.log(value);
-
-
-// function changeObject(objValue) {
-// 	console.log("in changeObject...");
-// 	console.log("before:");
-// 	console.log(objValue);
-
-// 	objValue.x = 5;
-// 	console.log("after:");
-// 	console.log(objValue);
-// }
-
-// value = {x: 7};
-// changeObject(value); // objValue = value
-// console.log("after changeObject, orig value:");
-// console.log(value);
-
-// //*** Lesson48
-
-// function test() {
-// 	console.log(this);
-// 	this.myName = "Julia";
-// }
-// test();
-// console.log(window.myName); // THIS
-
-// // Function constructors
-
-// function Circle(radius) {
-// 	this.radius = radius;
-// }
-// 	Circle.prototype.getArea =
-// 		function() {
-// 			return Math.PI *  Math.pow(this.radius, 2);		
-// }
-//  var myCircle = new Circle(10);
-//  console.log(myCircle);
-
-//  var myOtherCircle = new Circle(20);
-//  console.log(myOtherCircle);
-
- // *** Lesson49
-var literalCircle = {
-	radius: 10,
-
-	getArea: function() {
-		var self = this;
-		console.log(this);
-
-		var increaseRadius = function () {
-			self.radius = 20;
-		}
-		increaseRadius();
-		console.log(this.radius);
-
-		return Math.PI * Math.pow(this.radius, 2);
-	}
-}
-
-console.log(literalCircle.getArea());
-
-//*** Lesson50
-// Arrays
-
-// var array =  new Array();
-// array[0] = "Julia";
-// array[1] = 2;
-// array[2] = function(name) {
-// 	console.log("Hello " + name);
-// }
-// array[3] = {course: " HTML, CSS & JS"};
-
-// console.log(array);
-// array[2](array[0]);
-// console.log(array[3].course);
-
-// Short hand array creation
-// var names = ["Julia", "Anna", "Olga"];
-
-// console.log(names);
-
-// for (var i=0; i < names.length; i++) {
-// 	console.log("Hello "+ names[i]);
-// }
-
-// names[100]= "Sofia";
-// for (var i=0; i < names.length; i++) {
-// 	console.log("Hello "+ names[i]);
-// }
-
-var names2 = ["Julia", "Anna", "Olga"];
-
-var myObj = {
-	name: "Julia",
-	course: "HTML/CSS/JS",
-	platform: "Coursera"
-}
-
-for (var prop in myObj) {
-	console.log( prop + ": " + myObj[prop]);
-}
-
-for (name in names2){
-	console.log("Hello " + names2[name]);
-}
-
-// *** Lesson51
-
-//Closures
-
-function makeMultiplier(multiplier) {
-
-	function b(){
-	console.log("Multiplier is " + multiplier);
-}	
-b();
-
-return(
-		function(x) {
-			return multiplier * x;
-		}
-	);
-}
-
-var doubleAll = makeMultiplier(2);
-console.log(doubleAll(10));
-
-// *** Lesson52
-
-
+// // *******************************
+// // START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
+// // *******************************
+// //
+// // Module 4 Assignment Instructions.
+// //
+// // The idea of this assignment is to take an existing array of names
+// // and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// // The program should say "Hello" to any name except names that start with a "J"
+// // or "j", otherwise, the program should say "Good Bye". So, the final output
+// // on the console should look like this:
+// /*
+// Hello Yaakov
+// Good Bye John
+// Good Bye Jen
+// Good Bye Jason
+// Hello Paul
+// Hello Frank
+// Hello Larry
+// Hello Paula
+// Hello Laura
+// Good Bye Jim
+
+// WARNING!!! WARNING!!!
+// The code does NOT currently work! It is YOUR job to make it work
+// as described in the requirements and the steps in order to complete this
+// assignment.
+// WARNING!!! WARNING!!!
+
+// */
+
+// // STEP 1:
+// // Wrap the entire contents of script.js inside of an IIFE
+// // See Lecture 52, part 2
+// // (Note, Step 2 will be done in the SpeakHello.js file.)
+
+ //var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+// // STEP 10:
+// // Loop over the names array and say either 'Hello' or "Good Bye"
+// // using the 'speak' method or either helloSpeaker's or byeSpeaker's
+// // 'speak' method.
+// // See Lecture 50, part 1
+// for (/* fill in parts of the 'for' loop to loop over names array */) {
+
+//   // STEP 11:
+//   // Retrieve the first letter of the current name in the loop.
+//   // Use the string object's 'charAt' function. Since we are looking for
+//   // names that start with either upper case or lower case 'J'/'j', call
+//   // string object's 'toLowerCase' method on the result so we can compare
+//   // to lower case character 'j' afterwards.
+//   // Look up these methods on Mozilla Developer Network web site if needed.
+//   // var firstLetter =
+
+//   // STEP 12:
+//   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
+//   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+//   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+//   // name in the loop.
+ //  if (/* fill in condition here */) {
+ //    // byeSpeaker.xxxx
+ //  } else {
+ //   // helloSpeaker.xxxx
+ // }
+ //  }
+
+
+(function(){
+
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", 
+                  "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+    for (var i = 0; i < names.length; i++ ) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+
+      if (firstLetter === 'j') {
+          byeSpeaker.speak(names[i]);
+      } 
+      else {
+          helloSpeaker.speak(names[i]);
+      }
+    }
+
+
+})();
 
